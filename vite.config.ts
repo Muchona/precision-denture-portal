c89795b729c0d8b21bd6181fc14ced43469efce2
Muchona <mucho055@gmail.com>
2026-09-24 23:17:13 +0100

    fix: update base url to precision-denture-portal

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/monaghan-denture-portal/',
+  base: '/precision-denture-portal/',
   plugins: [
     react(),
     tailwindcss(),
